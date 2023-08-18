726c38cdc8b3c24c78a65285f5e040fbb1d74a67
Henrique <henrique1989@gmail.com>
2023-08-18 23:41:01 +0100

    Added postman and run docker command

--- run_docker.ps1
+++ run_docker.ps1
@@ -1,5 +1,2 @@
-# Run proxy-golang-api container in detached mode
-docker run -d --rm -p 8080:8080 --name proxy-golang-api proxy-golang-api
-
-# Run proxy-golang-psql container in detached mode
-docker run -d --rm -p 5432:5432 --name proxy-golang-psql proxy-golang-psql
+cd .\internal
+docker-compose up
